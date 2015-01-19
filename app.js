4a6e991a33ae217134fa922d036cb0b9f9c669ed
Timilehin Aderinola <timilehin.aderinola@andela.co>
2015-01-19 02:37:21 +0100

    Fixed for heroku

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ require('./config/db.js');
 var allowCrossDomain = function(request, response, next) {
   response.header('Access-Control-Allow-Origin', '*');
   response.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-Width, username');
+  response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
   next();
 };
